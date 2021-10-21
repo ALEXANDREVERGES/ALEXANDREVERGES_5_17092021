f840dd3f37f5ebb5f1bea225bbca7b859a47094e
Alexandre Verges <alexandre.verges@live.fr>
2021-10-21 16:11:54 +0200

    correction

--- front-end/JS/panier.js
+++ front-end/JS/panier.js
@@ -6,7 +6,7 @@ let myObj = JSON.stringify(produitPanier);
 localStorage.setItem("produitPanier", myObj);
 let myObj_deserialized = JSON.parse(localStorage.getItem("produitPanier"));
 
-console.log(myObj_deserialized);
+//console.log(myObj_deserialized);
 
 
 //annoncé si le panier est vide + photo
@@ -32,7 +32,7 @@ console.log(myObj_deserialized);
        
 
         <!---------- formulaire ------->
-        <form method="GET" action="order.html" onsubmit="return validateForm()" class="form">
+        <form onsubmit="return valideForm()" name="form"  class="form">
         <h2>Votre adresse de livraison</h2>
             <div class="">
                 <div class="bold" id="">
@@ -92,24 +92,97 @@ btnSupprimer.addEventListener("click", () => {
   localStorage.clear();
   location.reload();
 });
-const btnEnvoieForm = document.querySelector(".btnEnvoieForm ");
-btnEnvoieForm .addEventListener("click",(e) =>{
-    e.preventDefault();
+
     //définition d'une classe pour fabriquer l'objet dans lequel iront 
     //les values du form
 
-    class Formulaire {
-        constructor(prénom,nom){
-            this.prenom = document.querySelector("#user_name").value;
-            this.nom = document.querySelector("#user_lastname").value;
-            this.postal = document.querySelector("#user_postal").value;
-            this.ville = document.querySelector("#user_city").value;
-            this.adresse = document.querySelector("#user_adress").value;
-            this.mail= document.querySelector("#user_mail").value;
-            this.telephone= document.querySelector("#phone").value;
-        }
+    
+const btnEnvoieForm = document.querySelector(".btnEnvoieForm ");
+btnEnvoieForm .addEventListener("click",() =>{
+    
+    //définition d'une classe pour fabriquer l'objet dans lequel iront 
+    //les values du form
+    function valideForm(){    
+        var prenom = document.forms["form"]["user_name"];
+        var nom = document.forms["form"]["user_lastname"];
+        var postal = document.forms["form"]["user_postal"];
+        var ville = document.forms["form"]["user_city"];
+        var adresse = document.forms["form"]["user_adress"];
+        var mail = document.forms["form"]["user_mail"];
+        var telephone = document.forms["form"]["user_phone"];
+    
+if (prenom.value == "")                                  
+{ 
+    alert("Mettez votre prénom."); 
+    prenom.focus(); 
+    return false; 
+}    
+if (nom.value == "")                                  
+{ 
+    alert("Mettez votre nom."); 
+    nom.focus(); 
+    return false; 
+}    
+if (postal.value == "")                               
+{ 
+    alert("Mettez votre code postal."); 
+    postal.focus(); 
+    return false; 
+}  
+if (ville.value == "")                               
+{ 
+    alert("Mettez votre ville."); 
+    ville.focus(); 
+    return false; 
+}              
+if (adresse.value == "")                               
+{ 
+    alert("Mettez votre adresse."); 
+    adresse.focus(); 
+    return false; 
+}        
+if (mail.value == "")                                   
+{ 
+    alert("Mettez une adresse email valide."); 
+    mail.focus(); 
+    return false; 
+}    
+if (mail.value.indexOf("@", 0) < 0)                 
+{ 
+    alert("Mettez une adresse email valide."); 
+    mail.focus(); 
+    return false; 
+}    
+if (mail.value.indexOf(".", 0) < 0)                 
+{ 
+    alert("Mettez une adresse email valide."); 
+    mail.focus(); 
+    return false; 
+}    
+if (telephone.value == "")                           
+{ 
+    alert("Mettez votre numéro de téléphone."); 
+    phone.focus(); 
+    return false; 
+}    
+
+return true; 
+
+}
+
+ class Formulaire {
+    constructor(prenom,nom,postal,ville,adresse,mail,telephone){
+        this.prenom = document.querySelector("#user_name").value;
+        this.nom = document.querySelector("#user_lastname").value;
+        this.postal = document.querySelector("#user_postal").value;
+        this.ville = document.querySelector("#user_city").value;
+        this.adresse = document.querySelector("#user_adress").value;
+        this.mail= document.querySelector("#user_mail").value;
+        this.telephone= document.querySelector("#phone").value;
     }
-    const formulaireValues = new Formulaire();
-    console.log(formulaireValues);
-    localStorage.setItem("formulaire", JSON.stringify(formulaireValues));
+}
+const formulaireValues = new Formulaire();
+console.log(formulaireValues);
+localStorage.setItem("formulaire", JSON.stringify(formulaireValues));
+
 })
