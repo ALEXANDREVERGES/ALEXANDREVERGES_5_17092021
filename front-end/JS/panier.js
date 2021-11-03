3c641c4226e7b6d960ca9e46de059a67ae402eaa
Alexandre Verges <alexandre.verges@live.fr>
2021-11-03 17:20:21 +0100

    test correction

--- front-end/JS/panier.js
+++ front-end/JS/panier.js
@@ -1,15 +1,6 @@
 const basket = JSON.parse(localStorage.getItem("peluche")) || [];
 
-//affichage nb article dans panier sur page web
-let qtTotalCalcul = [];
-    for(var t=0; t < basket.length; t++){
-    articlePanier = basket[t].idQuantity;
-    qtTotalCalcul.push(articlePanier)
-    
-  }
-  let redu = (accumulator, currentValue) => accumulator + currentValue;
-  const qtTotal = qtTotalCalcul.reduce(redu);
-    let nbArticlePanier = document.getElementById("basketPreview").textContent = qtTotal;
+
 
 
 //CALCUL PRIX TOTAL DU PANIER 
