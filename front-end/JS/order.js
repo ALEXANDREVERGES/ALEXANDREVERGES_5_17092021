7aeb3ae2f9b6abf8faa79289a0f9c06bc2e9fe97
Alexandre Verges <alexandre.verges@live.fr>
2021-11-09 15:45:02 +0100

    correction

--- front-end/JS/order.js
+++ front-end/JS/order.js
@@ -7,8 +7,9 @@ let orderId = localStorage.getItem("order");
 
 const contact = document.getElementById("contact");
 contact.innerHTML =`
+
 <div class="container_order">
-<div class="order"> Félicitations <strong>${order.firstname}!!!</strong><br> Votre numéro de commande  est :"${orderId}" </div> <br>
+<div class="order">Félicitations <strong>${order.firstname}!</strong> <br>Votre numéro de commande  est :"${orderId}" </div> <br>
 Pour un montant total de : <strong>${priceOfOrder}</strong> ! <br>
 Elle arrivera chez vous, au "<strong>${order.adress} ${order.postal} ${order.city}</strong>". <br>
 Vous allez recevoir un mail sur <strong>${order.email}</strong> lorsque votre commande sera envoyée. <br>
